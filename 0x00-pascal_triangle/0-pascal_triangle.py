cc020eb15694f65d99bcbef95bdbd31d80dbe30e
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-01 05:57:22 +0100

    refactored code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+""" pascal_triangle """
+
 def pascal_triangle(n):
+    """ returns a pascal triangle """
     triangle = [[1] * (i + 1) for i in range(n)]
     for i in range(n):
         for j in range(1, i):
