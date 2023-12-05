c88b8325044d7b17d2937444df57d038068a3330
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-05 18:40:33 +0100

    debugging code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """lockboxes"""
 
+
 def canUnlockAll(boxes):
     """a method that determines if all the boxes can be opened"""
 
-    if not boxes or len(boxes) == 1:
-        return True
+    # if not boxes or len(boxes) == 1:
+    #     return True
 
     keys = set(boxes[0])
     for idx in range(1, len(boxes)):
