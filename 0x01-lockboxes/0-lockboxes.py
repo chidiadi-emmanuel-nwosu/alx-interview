8207c169872e92a9e90efec066dfcf168ebef4a3
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-08 09:03:58 +0100

    refactored code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,17 +5,15 @@
 def canUnlockAll(boxes):
     """a method that determines if all the boxes can be opened"""
 
-    if not isinstance(boxes, list) or any(not isinstance(box, list) for box in boxes):
-        return False
-
     keys = set(boxes[0])
     for idx in range(1, len(boxes)):
         if idx in keys:
-            keys |= set(list(boxes[idx]))
+            keys |= set(boxes[idx])
         else:
-            tmp = set([])
+            tmp = set()
             for key in keys:
-                tmp |= set(list(boxes[key]))
+                if key < len(boxes):
+                    tmp |= set(boxes[key])
             if idx not in tmp:
                 return False
 
