22d91a565732e1ff0560194d3d7c9e9b60217d04
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-18 15:52:26 +0100

    fixed error for starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const id = process.argv.slice(2);
 const url = `https://swapi.dev/api/films/${id}`;
 
-const func = async () => {
+const starWars = async () => {
   try {
     const body = await new Promise((resolve, reject) => {
       request(url, (error, response, body) => {
@@ -16,13 +16,13 @@ const func = async () => {
       });
     });
 
-    const jsonRes = JSON.parse(body);
+    const res = JSON.parse(body);
 
-    for (const characterUrl of jsonRes.characters) {
-      const body2 = await new Promise((resolve, reject) => {
-        request(characterUrl, (err, res, body) => {
-          if (err) {
-            reject(err);
+    for (const characterUrl of res.characters) {
+      const body = await new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
           } else {
             resolve(body);
           }
@@ -30,15 +30,15 @@ const func = async () => {
       });
 
       try {
-        const jres = JSON.parse(body2);
-        console.log(jres.name);
+        const res = JSON.parse(body);
+        console.log(res.name);
       } catch (parseError) {
         console.error('Error parsing JSON:', parseError);
       }
     }
-  } catch (err) {
-    console.error('Error:', err);
+  } catch (error) {
+    console.error('Error:', error);
   }
 };
 
-func();
+starWars();
