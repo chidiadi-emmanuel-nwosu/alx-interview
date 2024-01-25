450cfa578361a601380c52ff7cb6451b748e50b6
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-25 22:04:27 +0100

    fix error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-0-rotate_2d_matrix module
+rotate_2d_matrix module;
+provides a function for rotating a 2D matrix.
 """
 
 
 def rotate_2d_matrix(matrix: list[list[int]]) -> None:
     """
-    rortates a 2D matrix
+    Rotates a given 2D matrix in-place.
+
+    Args:
+        matrix: The input 2D matrix to be rotated.
     """
     length = len(matrix)
 
     for i in range(length):
         for j in range(i + 1, length):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
 
     for row in matrix:
         row.reverse()
