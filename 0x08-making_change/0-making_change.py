abb71c4a72d9b48a5806718f87e5c759823da639
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-02-01 22:07:44 +0100

    fixed pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,8 +3,21 @@
 0-making_change
 """
 
+
 def makeChange(coins, total):
-    """makeChange
+    """
+    Calculates the minimum number of coins required to
+    make change for a given total amount.
+
+    Args:
+    - coins (list of int): Available denominations of coins.
+    - total (int): Total amount for which change needs to be made.
+
+    Returns:
+    - int: The minimum number of coins required to
+           make change for the total amount.
+           Returns -1 if it's not possible to make
+           change with the given coins.
     """
     if total <= 0:
         return 0
